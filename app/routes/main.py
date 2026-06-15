8e0f73bbb7de7908990eaf2dc2ca95000db76a37
malabamalaba26 <malabamalaba26@gmail.com>
2026-06-15 15:24:23 +0300

    Read email configuration from environment variables

--- app/routes/main.py
+++ app/routes/main.py
@@ -116,21 +116,20 @@ except:
     SMS_AVAILABLE = False
 
 
-# Flask-Mail Configuration
+# Flask-Mail Configuration - Read from environment variables
 try:
     from flask_mail import Mail, Message
-
     MAIL_AVAILABLE = True
 except:
     MAIL_AVAILABLE = False
 
-MAIL_SERVER = "smtp.gmail.com"
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = "malabamalaba26@gmail.com"
-MAIL_PASSWORD = "gjgbawyjlciutgrd"
-MAIL_DEFAULT_SENDER = "malabamalaba26@gmail.com"
-APP_URL = "http://localhost:5000"
+MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True').lower() == 'true'
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'malabamalaba26@gmail.com')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '')
+MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', MAIL_USERNAME)
+APP_URL = os.environ.get('APP_URL', 'http://localhost:5000')
 
 mail = Mail()
 
